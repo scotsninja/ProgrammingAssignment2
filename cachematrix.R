c5e1146e0bb4318d97fb2e28506d30721301fa44
Kyle Knox <scotsninja@live.com>
2015-04-21 15:02:52 -0500

    - adding comments/cleanup

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculate and store the inverse of a matrix so that it may be accessed quickly
+## without requiring re-calculation
 
-## Write a short comment describing this function
+## Object for storing a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   
+  # set the base matrix
   set <- function(y){
     x <<- y
     m <<- NULL
   }
   
+  # return the base matrix
   get <- function() x
   
+  # store the inverted matrix
   setinverse <- function(solve) m <<- solve
   
+  # return the inverted matrix
   getinverse <- function() m
   
   list(set = set, get = get,
@@ -23,18 +27,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Compute matrix inverse using solve(),
+## loading from cached value if one exists
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
+  
   if(!is.null(m)){
     message("loading cached data")
     return(m)
   }
   
+  # calculate inverted matrix, then store
   data <- x$get()
   m <- solve(data, ...)
   x$setinverse(m)
+  
   m
 }
--- run_tests.R
+++ run_tests.R
@@ -1,3 +1,5 @@
+## Run tests using testhat library on 2x2 and 4x4 matrices
+
 library('testthat')
 
 source('cachematrix.R')
